7d1323ad968253be1d80f573ae789245ef5cd870
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2022-12-03 19:56:57 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2==1)     {         printf("weird");     }     else if(n%2==0 && n>=2 && n<=5)     {         printf("not weird");     }     else if(n%2==0 && n>=6 && n<=20)     {         printf("weird");     }     else if(n%2==0 && n>=20)     {         printf("not werid");     } }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==1)
+    {
+        printf("weird");
+    }
+    else if(n%2==0 && n>=2 && n<=5)
+    {
+        printf("not weird");
+    }
+    else if(n%2==0 && n>=6 && n<=20)
+    {
+        printf("weird");
+    }
+    else if(n%2==0 && n>=20)
+    {
+        printf("not werid");
+    }
+}
