a5103a9d1d7cacf28145da330a33b8de0dc675de
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2022-12-19 17:44:43 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],b,c,d,e,f,max=0,i;     for(b=0;b<n;b++)     {         scanf("%d",&a[b]);     }     max=a[i];     for(c=0;c<n;c++)     {         if(a[c]<max)         {             max=a[c];         }     }     //printf("%d",max);     for(max;max>0;max--)     {         i=0;         for(d=0;d<n;d++)         {             if(a[d]%max==0)             {                 i++;             }         }         if(i==n)         {             printf("%d",max);             break;         }     } }

---
+++ Minimum_Scale_length.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],b,c,d,e,f,max=0,i;
+    for(b=0;b<n;b++)
+    {
+        scanf("%d",&a[b]);
+    }
+    max=a[i];
+    for(c=0;c<n;c++)
+    {
+        if(a[c]<max)
+        {
+            max=a[c];
+        }
+    }
+    //printf("%d",max);
+    for(max;max>0;max--)
+    {
+        i=0;
+        for(d=0;d<n;d++)
+        {
+            if(a[d]%max==0)
+            {
+                i++;
+            }
+        }
+        if(i==n)
+        {
+            printf("%d",max);
+            break;
+        }
+    }
+}
