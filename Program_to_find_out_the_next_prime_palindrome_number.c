20d64d8519c86844917c8c5d348d0a5411d556d6
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2022-12-19 20:46:49 +0530

    #include<stdio.h> int main() {     int n,i,j,z,k,b,s,count;     scanf("%d",&n);     for(i=n+1;i>0;i++)     {         s=0;         for(j=i;j>0;j=j/10)         {             b=j%10;             s=s*10+b;         }         if(s==i)         {             count=0;             for(z=1;z<=s;z++)             {                 if(s%z==0)                 count++;             }             if(count==2)             {                 printf("%d",s);                 break;             }         }     } }

---
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,z,k,b,s,count;
+    scanf("%d",&n);
+    for(i=n+1;i>0;i++)
+    {
+        s=0;
+        for(j=i;j>0;j=j/10)
+        {
+            b=j%10;
+            s=s*10+b;
+        }
+        if(s==i)
+        {
+            count=0;
+            for(z=1;z<=s;z++)
+            {
+                if(s%z==0)
+                count++;
+            }
+            if(count==2)
+            {
+                printf("%d",s);
+                break;
+            }
+        }
+    }
+}
