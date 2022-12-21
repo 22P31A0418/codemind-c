d8854157765cd57a262c0674982b7565b5f7acb9
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2022-12-21 18:26:34 +0530

    #include<stdio.h> int main() {     int l,n,w,h,i;     scanf("%d%d",&l,&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&w,&h);         if(w<l||h<l)         printf("UPLOAD ANOTHER ");         else if(w==h)         printf("ACCEPTED ");         else         printf("CROP IT ");     } }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,w,h,i;
+    scanf("%d%d",&l,&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&w,&h);
+        if(w<l||h<l)
+        printf("UPLOAD ANOTHER
+");
+        else if(w==h)
+        printf("ACCEPTED
+");
+        else
+        printf("CROP IT
+");
+    }
+}
