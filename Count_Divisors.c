c119dab77a6861c4b436bd6a4c3fe2a6487fae03
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2022-12-21 18:30:03 +0530

    #include<stdio.h> int main() {     int l,r,k,i,count=0;     scanf("%d%d%d",&l,&r,&k);     for(i=l;i<=r;i++)     {         if(i%k==0)         count++;     }     printf("%d",count); }

---
+++ Count_Divisors.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int l,r,k,i,count=0;
+    scanf("%d%d%d",&l,&r,&k);
+    for(i=l;i<=r;i++)
+    {
+        if(i%k==0)
+        count++;
+    }
+    printf("%d",count);
+}
