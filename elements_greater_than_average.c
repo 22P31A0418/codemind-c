11641ab6af57f2dd8f6d16789d39d9d447c0cbb1
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2022-12-24 20:55:05 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,s=0,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         sum+=arr[i];     }     for(i=0;i<n;i++)     {         if(arr[i]>=sum/n)         s++;     }     printf("%d",s); }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,s=0,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=sum/n)
+        s++;
+    }
+    printf("%d",s);
+}
