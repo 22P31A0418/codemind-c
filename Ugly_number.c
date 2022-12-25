ad4417b0ec24a96fd0b266af1ebd0b4b8cd83d6f
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2022-12-25 20:56:50 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a==1 ||a==2 ||a==3 ||a==4 ||a==5 ||a==6 ||a==8 ||a==9 ||a==10 ||a==12 ||a==15)     {         printf("Ugly Number");     }     else     {         printf("Not Ugly Number");     } }

---
+++ Ugly_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==1 ||a==2 ||a==3 ||a==4 ||a==5 ||a==6 ||a==8 ||a==9 ||a==10 ||a==12 ||a==15)
+    {
+        printf("Ugly Number");
+    }
+    else
+    {
+        printf("Not Ugly Number");
+    }
+}
