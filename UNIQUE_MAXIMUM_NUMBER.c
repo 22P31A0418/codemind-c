ab35f345d26b65905d40f95cbae2b98b74871674
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2023-01-19 20:03:28 +0530

    #include<stdio.h> int main() {     int i,j,k,a[100],b[100],n,max,l=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         k=0;         for(j=0;j<n;j++)         {             if(a[i]==a[j])             {                 k++;             }         }         //printf("%d",k);         if(k==1)         {             b[l]=a[i];             //printf("%d",b[l]);             l++;         }     }     if(l!=0)     {         max=b[l];         for(i=0;i<l;i++)         {             if(b[i]>max)             {                 //printf("%d",max);                 max=b[i];             }         }         printf("%d",max);     }     else     {         printf("-1");     } }

---
+++ UNIQUE_MAXIMUM_NUMBER.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,a[100],b[100],n,max,l=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        k=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                k++;
+            }
+        }
+        //printf("%d",k);
+        if(k==1)
+        {
+            b[l]=a[i];
+            //printf("%d",b[l]);
+            l++;
+        }
+    }
+    if(l!=0)
+    {
+        max=b[l];
+        for(i=0;i<l;i++)
+        {
+            if(b[i]>max)
+            {
+                //printf("%d",max);
+                max=b[i];
+            }
+        }
+        printf("%d",max);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
