edc9dd44749cdfd4448c5ea36255d3ba2d4f13c4
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2023-08-05 11:07:55 +0530

    #include<stdio.h> int main() {     int n,m,arr1[100],arr2[100],i,j,temp,s;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr1[i]);     }     scanf("%d",&m);     for(i=0;i<m;i++)     {         scanf("%d",&arr2[i]);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr2[i]>arr2[j])             {                 temp=arr2[i];                 arr2[i]=arr1[i];                 arr2[j]=temp;             }         }     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr1[i]>arr1[j])             {                 temp=arr1[i];                 arr1[i]=arr1[j];                 arr1[j]=temp;             }         }     }     for(i=0;i<n;i++)     {         if(arr1[i]!=arr2[i])         {             printf("False");             s=1;             break;         }     }     if(s!=1)     printf("True"); }

---
+++ Make_Two_Arrays_Equal_by_Reversing_Sub-arrays.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,arr1[100],arr2[100],i,j,temp,s;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr1[i]);
+    }
+    scanf("%d",&m);
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&arr2[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr2[i]>arr2[j])
+            {
+                temp=arr2[i];
+                arr2[i]=arr1[i];
+                arr2[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr1[i]>arr1[j])
+            {
+                temp=arr1[i];
+                arr1[i]=arr1[j];
+                arr1[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr1[i]!=arr2[i])
+        {
+            printf("False");
+            s=1;
+            break;
+        }
+    }
+    if(s!=1)
+    printf("True");
+}
