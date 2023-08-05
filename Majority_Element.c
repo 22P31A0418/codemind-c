b958d467fe589c12d0f59da6f49892cbb21b1815
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2023-08-05 11:16:32 +0530

    #include<stdio.h> int main() {     int a[100],i,j,c,n,d,max;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(i!=j)             {                 if(a[i]==a[j])                 {                     c++;                 }             }         }         if(c>max)         {             max=c;             d=a[i];         }     }     printf("%d",d); }

---
+++ Majority_Element.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,j,c,n,d,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+            }
+        }
+        if(c>max)
+        {
+            max=c;
+            d=a[i];
+        }
+    }
+    printf("%d",d);
+}
